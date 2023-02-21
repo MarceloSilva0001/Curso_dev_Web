b64b10f003969a4b1750c0a57de990549cee8740
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-02-21 09:42:53 -0300

    remoção modulo d

--- Curso_Java_script_basico/moduloD/aula11.js
+++
@@ -1,8 +0,0 @@
-var vel = 50
-var carro = 70
-
-if(carro < vel || carro === vel){
-    console.log(`a velocidade do seu carro é ${carro} Km`)
-}else{
-    console.log(`a velocidade do seu carro é ${carro} Km você está Multado`)
-}
