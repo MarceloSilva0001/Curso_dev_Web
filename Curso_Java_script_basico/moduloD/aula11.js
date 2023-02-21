c6c60927253d887771150d3621a73c92a3c2c8d9
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-02-21 09:47:32 -0300

    voltando com modulo d

---
+++ Curso_Java_script_basico/moduloD/aula11.js
@@ -0,0 +1,8 @@
+var vel = 50
+var carro = 70
+
+if(carro < vel || carro === vel){
+    console.log(`a velocidade do seu carro é ${carro} Km`)
+}else{
+    console.log(`a velocidade do seu carro é ${carro} Km você está Multado`)
+}
---
+++ Curso_Java_script_basico/moduloD/aula12.js
@@ -0,0 +1,53 @@
+//  //Opção de Voto
+//  var idade = 2
+//  if(idade < 16 ){
+//      console.log('Não vota')
+//  } else if(idade < 18 || idade >= 65){
+//      console.log('Voto Opecional')
+//  } else{
+//      console.log('Voto Obrigatorio')
+//  }
+ 
+// ---------------------------------
+//Bom dia Boa tarde boa noite
+// var agora = new Date()
+// var hora = agora.getHours
+
+// console.log(hora)
+// if(hora > 0 && hora < 12){
+//     console.log('Bom dia')
+// }else if(hora >= 12 && hora <= 18){
+//     console.log('Boa tarde')
+// }else{
+//     console.log('Boa noite')
+// }
+
+// -------------------------------
+// Estrutura de multipla escolha
+// var diasem = 10
+
+// switch(diasem){
+//     case 0:
+//         console.log('Domingo')
+//         break
+//     case 1:
+//         console.log('Segunda')
+//         break
+//     case 2: 
+//         console.log('Terça')
+//         break
+//     case 3:
+//         console.log('Quarta')
+//         break
+//     case 4:
+//         console.log('Quinta')
+//         break
+//     case 5:
+//         console.log('Sexta')
+//         break
+//     case 6:
+//         console.log('Sabado')
+//         break 
+//     default:
+//         console.log('Selecione um dia da semana')
+// }
