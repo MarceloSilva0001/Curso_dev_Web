0a16599b43e648f8221551a1ad65499c7f40a2a1
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-02-21 10:41:49 -0300

    comentando no código mod D

--- Curso_Java_script_basico/moduloD/exerjs3.js
+++ Curso_Java_script_basico/moduloD/exerjs3.js
@@ -9,16 +9,29 @@ function anodenascimento(){
  if(fano.value.lenght == 0 || fano.value > ano){
     window.alert('erro')
  }else{
-   // window.alert('else')
+    //recebe a escolha do checbox e armazena na variavel
     var fsex = document.getElementsByName('sexo')
+
+    // Fazendo a subtração do valor ano atual menos o ano de nascimento, e armazeando dentro da variavel idade
     var idade = ano - Number(fano.value)
+
+    //Atribuidno a variavel genero uma valor de string vazio
     var genero = ''
+
+    //como no getElementById, que seleciona uma tag que existe a guarda na variavel, "document.createElement('tag')", cria uma tag e armazena ela dentro de uma variavel como fazemos como é feito da outra forma, resumindo ele cria uma tag
     var img = document.createElement('img')
+
+    //Aqui está sendo atruibuido um id para a tag que foi criada com o "createElement('img')", como nas tag normais é importante o id para identificação e como é foi criada atraves do js, pelo js colocamos o id vinculado a tag
     img.setAttribute('id','foto')
+
+    //essa condição valida se oque está amarzenado no na variavel fsex na primeira possição [0] é o atributo masculuno.
+    //Como se interpreta essa parte? é assim: se estiver marcado o check box da possição 0 que é a primeira faz esse bloco. 
         if(fsex[0].checked){
             genero =  'Homem'
             if(idade >= 0 && idade < 12){
+    //aqui a tag 'img' que criamos pelo js, recebe setAttribute, que significa atribuição de elemento, abre parestes('o atributo vai ser o 'src') de imagem e depois coloca se , para separar e passa o caminho da imagem.
                img.setAttribute('src', 'm.criança.jpg')
+    // se o bloco for true, vai alterar a cor do back ground.
                document.body.style.background = 'rgba(0, 38, 255, 0.616)'
             }else if(idade >=12 && idade < 18){
                 img.setAttribute('src', 'm.adolecente10.15.jpg')
@@ -33,6 +46,7 @@ function anodenascimento(){
             genero =  'Mulher'
             if(idade >= 0 && idade < 12){
                 img.setAttribute('src', 'f.criança.jpg')
+
                 document.body.style.background = 'rgba(255, 0, 191, 0.932)'
             }else if(idade >=12 && idade < 18){
                 img.setAttribute('src', 'f.adolecente10.15.jpg')
@@ -44,7 +58,9 @@ function anodenascimento(){
                 img.setAttribute('src', 'caveira.jpg')
             }
         }
+    //aqui dentro da tag res dentro do html vai mostrar essas informações
     res.innerHTML = 'Detectamos um '+genero+' com '+idade+' anos.'
+    // aqui está sendo adicionado um elemento que no caso vai ser a tag de imagem, a div com id res, bem parecido como fazemos quando vamos mostrar texto.
     res.appendChild(img)
  }
 }
