51ebb7d41f95e526f06a78460f47eb8b6e60bf99
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-02-22 11:31:17 -0300

    alterações

---
+++ Curso_Java_script_basico/moduloE/exAula/aula13.js
@@ -0,0 +1,13 @@
+function clique(){
+   
+}
+var c = 1
+//Faça o o bloco, depois Faça o bloco enquanto laço de repetição for true
+do{
+   console.log('numero '+c++) 
+}while(c <= 10)
+//Faça o bloco enquanto laço de repetição for true
+while(c <= 10){
+   console.log('numero '+c--) 
+}
+
