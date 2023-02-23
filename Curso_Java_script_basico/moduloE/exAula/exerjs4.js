071dcd6ebc95e33ec0f70e1a583969db722a5a16
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-02-23 17:42:28 -0300

    primeira parte

--- Curso_Java_script_basico/moduloE/exAula/exerjs4.js
+++ Curso_Java_script_basico/moduloE/exAula/exerjs4.js
@@ -1,21 +1,22 @@
- function contar(){
-     var inicio = document.getElementById('fim')
-     var fim = document.getElementById('fim')
-     var pulo = document.getElementById('fim')
+  function contar(){
+     var inicio = document.getElementById('inicio').value
+     var fim = document.getElementById('fim').value
+     var pulo = document.getElementById('pulo').value
      var res = document.getElementById('res')
+
      inicio = Number(inicio)
-     fim = Number(fim.value)
-     pulo = Number(pulo.value)
-    var inicio = 1
-    var fim = 10
+     fim = Number(fim)
+     pulo = Number(pulo)
+
+   //  res.innerHTML = `${inicio}/ ${fim}/ ${pulo}`
 
     if(pulo >= 2){
          for(inicio=inicio;inicio<=fim;inicio+=pulo){
-               document.getElementById('res').innerHTML += inicio + '<br>'+'&#x1F449'
+          res.innerHTML +=inicio+'&#x1F449';
             }
     }else{
          for(inicio=inicio;inicio<=fim;inicio++){
-            document.getElementById('res').innerHTML += inicio + ' '+'&#x1F449'
+          res.innerHTML +=inicio+'&#x1F449';
            }
     }
 }
