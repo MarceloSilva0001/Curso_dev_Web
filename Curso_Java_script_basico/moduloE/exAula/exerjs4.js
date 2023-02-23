66750caa5bb360fbcf768fa4259f65e7a790acb6
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-02-23 15:29:40 -0300

    exercício mod e
    exercício, fazendo sozinho

--- Curso_Java_script_basico/moduloE/exAula/exerjs4.js
+++ Curso_Java_script_basico/moduloE/exAula/exerjs4.js
@@ -1,16 +1,21 @@
-function contar(){
-    var inicio = document.getElementById('fim').value
-    var fim = document.getElementById('fim').value
-    var pulo = document.getElementById('fim').value
-    var res = document.getElementById('res')
-    inicio = Number(inicio)
-    fim = Number(fim)
-    pulo = Number(pulo)
+ function contar(){
+     var inicio = document.getElementById('fim')
+     var fim = document.getElementById('fim')
+     var pulo = document.getElementById('fim')
+     var res = document.getElementById('res')
+     inicio = Number(inicio)
+     fim = Number(fim.value)
+     pulo = Number(pulo.value)
+    var inicio = 1
+    var fim = 10
 
-    for(inicio=inicio;inicio<=fim;inicio++){
-       console.log(inicio)
+    if(pulo >= 2){
+         for(inicio=inicio;inicio<=fim;inicio+=pulo){
+               document.getElementById('res').innerHTML += inicio + '<br>'+'&#x1F449'
+            }
+    }else{
+         for(inicio=inicio;inicio<=fim;inicio++){
+            document.getElementById('res').innerHTML += inicio + ' '+'&#x1F449'
+           }
     }
-
-   // document.getElementById('res').innerHTML = `${inicio}/ ${pulo}/ ${pulo}/`
-    
 }
---
+++ Curso_Java_script_basico/moduloE/exAula/node_e5e9cb51dad61.tmp.js
@@ -0,0 +1,18 @@
+// function contar(){
+//     var inicio = document.getElementById('fim').value
+//     var fim = document.getElementById('fim').value
+//     var pulo = document.getElementById('fim').value
+//     var res = document.getElementById('res')
+//     inicio = Number(inicio)
+//     fim = Number(fim)
+//     pulo = Number(pulo)
+    var inicio = 1
+    var fim = 10
+
+    for(inicio=inicio;inicio<=fim;inicio+2){
+       console.log(inicio)
+    }
+
+   // document.getElementById('res').innerHTML = `${inicio}/ ${pulo}/ ${pulo}/`
+    
+// }
