abfb21c5f089f3e2b7bf79efb0d4121678be58ba
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-02-24 11:28:16 -0300

    .

---
+++ Curso_Java_script_basico/moduloE/exAula/exerjs5.js
@@ -0,0 +1,22 @@
+function contar(){
+    var i = document.getElementById('i').value
+    var p = document.getElementById('p').value
+    var f = document.getElementById('f').value
+
+    var res = document.getElementById('res')
+    i = Number(i)
+    p = Number(p)
+    f = Number(f)
+
+    if(i <= 0 || p <= 0 || f <=0){
+        res.innerHTML =    'Nenhum campo pode estar vazio Ou ter o valor 0'
+    }else if(i<f){
+        for(var c = i; c <= f; c += p){
+            res.innerHTML += `> ${c} `
+        }
+    }else{
+        for(var c = i; c >= f; c -= p ){
+            res.innerHTML += `> ${c} `
+        }
+    }
+}
