7bd7bb325fe762ac33e2014a3671a425db00428e
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-02-28 11:19:05 -0300

    exercicio

---
+++ Curso_Java_script_basico/moduloF/Exercio/aula15.js
@@ -0,0 +1,10 @@
+let num = [1,6,5,4,8];
+num [5] = 6;
+num.sort();
+num.push(7);
+ 
+console.log(num)
+for(let i = 0;i<=num.length;i++){
+    console.log(num[i]);
+   
+}
---
+++ Curso_Java_script_basico/moduloF/Exercio/node_8917679217de4.tmp.js
@@ -0,0 +1,8 @@
+let num = [1,6,5,4,8];
+num [5] = 6;
+num.sort();
+num.push(7);
+
+while(num>=num){
+    console.log(num[0]);
+}
