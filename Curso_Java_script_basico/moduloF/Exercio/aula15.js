36a85628f7f131837591eec389f311fe68f2ab19
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-03-02 11:10:06 -0300

    aula 15 concluida

--- Curso_Java_script_basico/moduloF/Exercio/aula15.js
+++ Curso_Java_script_basico/moduloF/Exercio/aula15.js
@@ -1,6 +1,27 @@
 let num = [4,5,6,4,8,0,2,0];
 
+
+console.log(`\nVetor original: ${num} \n`)
+console.log(`Vetor em Ordem Crescente: ${num.sort()} \n`)
+
+num[6]= 9;
+console.log(`Adicionando elemento 9 na posição 6 do indice:${num} \n`)
+
+console.log(`O comprimeto do veto é: ${num.length} Elementos \n`)
+var val = 8
+var pos = num.indexOf(val)
+    if(pos == -1){
+         console.log(`Ò valor ${val} Não está no vetor\n \n \n `)
+    }else{
+        console.log(`Ò valor ${val} está na possição ${pos} do indice \n \n \n \n`)
+    }
+
+
+
 for(var i = 0; i <= num.length;i++){
-    console.log(`${num[i]}`)
+    console.log(`Posição ${i}- Elemento > ${num[i]}`);
 }
-//teste observação
+console.log("---------------------")
+for(let ps in num ){
+    console.log(`Posição ${ps}Elemento >  ${num[ps]}`);
+}
