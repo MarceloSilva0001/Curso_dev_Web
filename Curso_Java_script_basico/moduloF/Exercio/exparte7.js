7848f31acc2b508d8c52b3d7bf2ca56664707259
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-03-03 17:16:03 -0300

    cmt

--- Curso_Java_script_basico/moduloF/Exercio/exparte7.js
+++ Curso_Java_script_basico/moduloF/Exercio/exparte7.js
@@ -1,12 +1,33 @@
-    var res = document.getElementById("res");
+    var reslist = document.getElementById("res");
+    var res2 = document.getElementById("re2");
     var num =  document.getElementById("num");
+    let valores = [];
+
+function  isNumero(n){
+    if(Number(n)>=1 && Number(n) <= 100){
+        return true;
+    }else{
+    return false;
+    }
+}
+
+function  inlist(n,l){
+    if(l.indexOf(Number(n)) != -1){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function adicionar(){
+    //só vai adicionar se o valor estiver o numero em num && e só vai adcionar se ele "!" não estiver na lista
+    if(isNumero(num.value) && !inlist(num.value, valores)){
+        window.alert("Tudo ok")
+    }else{
+        window.alert("Valor inválido ou já encontrado na lista")
+    }
+}
+
     
    
     
-
-function adicionar(){
-    num = Number(num.value);
-    var NumOption = document.createElement("option");
-    NumOption.textContent = num;
-    res.appendChild(NumOption);
-}
