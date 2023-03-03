923a068d881cdfe93f43f7f58431f141bc0d935c
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-03-03 17:43:44 -0300

    exlusão

--- Curso_Java_script_basico/moduloF/Exercio/exparte7.js
+++ Curso_Java_script_basico/moduloF/Exercio/exparte7.js
@@ -3,31 +3,28 @@
     var num =  document.getElementById("num");
     let valores = [];
 
-function  isNumero(n){
-    if(Number(n)>=1 && Number(n) <= 100){
-        return true;
-    }else{
-    return false;
-    }
-}
 
-function  inlist(n,l){
-    if(l.indexOf(Number(n)) != -1){
-        return true;
-    }else{
-        return false;
-    }
-}
 
-function adicionar(){
-    //só vai adicionar se o valor estiver o numero em num && e só vai adcionar se ele "!" não estiver na lista
-    if(isNumero(num.value) && !inlist(num.value, valores)){
-        window.alert("Tudo ok")
-    }else{
-        window.alert("Valor inválido ou já encontrado na lista")
-    }
-}
 
-    
-   
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
