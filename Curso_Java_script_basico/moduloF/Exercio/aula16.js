789068bf0eb047333a26297ed249b36650399979
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-03-02 23:49:00 -0300

    ex parte 7

---
+++ Curso_Java_script_basico/moduloF/Exercio/aula16.js
@@ -0,0 +1,14 @@
+function teste(n){
+   
+if(n%2 == 0){
+    return 'par';
+}else{
+    return 'impar';
+}
+}
+console.log(teste(5))
+
+function teste2(a, b){
+    return  a+b;
+}
+console.log("A soma entre os valores é: "+teste2(2,5))
