f2a15e3c0940e7e005b6c1a1f8ffcb2d6b813491
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-03-04 23:32:18 -0300

    .

---
+++ Curso_Java_script_basico/moduloF/Exercio/exparte8.js
@@ -0,0 +1,34 @@
+var reslist = document.getElementById("res");
+var res2 = document.getElementById("re2");
+var num =  document.getElementById("num");
+//declaração de vetor
+let valores = [];
+//a função verifica se o valor que está dentro de num está dentre 1 e 100
+function  isNumero(n){
+if(Number(n)>=1 && Number(n) <= 100){
+    return true;
+}else{
+return false;
+}
+}
+//Essa função verifica se o valor está dentro de vetor, se o valor digitado for diferente de -1(valor dado quando o valor digitado não tem idice valido, indice valido começa apartir de 0 e vai somando)
+function  inlist(n,l){
+if(l.indexOf(Number(n)) != -1){
+    return true;
+}else{
+    return false;
+}
+}
+
+function adicionar(){
+//só vai adicionar se o valor estiver o numero em num && e só vai adcionar se ele "!" não estiver na lista
+if(isNumero(num.value) && !inlist(num.value, valores)){
+    window.alert("Tudo ok")
+}else{
+    window.alert("Valor inválido ou já encontrado na lista")
+}
+}
+
+
+
+
