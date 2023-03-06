bf1032b65c6aac12d1bb16dd6b13ad947f30e127
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-03-06 11:23:24 -0300

    cmt

--- Curso_Java_script_basico/moduloF/Exercio/exparte8.js
+++ Curso_Java_script_basico/moduloF/Exercio/exparte8.js
@@ -23,10 +23,26 @@ if(l.indexOf(Number(n)) != -1){
 function adicionar(){
 //só vai adicionar se o valor estiver o numero em num && e só vai adcionar se ele "!" não estiver na lista
 if(isNumero(num.value) && !inlist(num.value, valores)){
-    window.alert("Tudo ok")
-}else{
+    //adiciona o valor contido no input "num" ao vetor chamado valores, o ,push adiiona o valor na ultima posição do vetor, então sempre que adicionarem valor no input, ele vai para ultima posição do vertor pois colocamos o nome do vetor ".push"
+    valores.push(Number(num.value));
+
+    //Criando uma tag option 
+    var option = document.createElement("option");
+
+    //atribuindo o tipo de elemento a tag option, que vai ser o conteudo que ela recebe, o conteudo é o valor num que está no input
+    option.text = `Adicionado: ${num.value}`;
+
+    //atrelando a opção a lista a uma elemento, nesse caso o select
+    reslist.appendChild(option);
+
+    }else{
     window.alert("Valor inválido ou já encontrado na lista")
-}
+    }
+    //faz o input receber o valor "nada"
+    num.value = '';
+    //faz com que após clica no adicionar o cursor volte ao compo de input
+    num.focus();
+
 }
 
 
