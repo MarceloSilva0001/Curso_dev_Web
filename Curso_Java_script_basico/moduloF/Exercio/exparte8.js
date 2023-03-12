70417d3baeacf5dac58e4d9c525c7ba480d85604
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2023-03-12 19:03:10 -0300

    cmt

--- Curso_Java_script_basico/moduloF/Exercio/exparte8.js
+++ Curso_Java_script_basico/moduloF/Exercio/exparte8.js
@@ -3,7 +3,7 @@ var res2 = document.getElementById("res2");
 var num =  document.getElementById("num");
 //declaração de vetor
 let valores = [];
-//a função verifica se o valor que está dentro de num está dentre 1 e 100
+//a função verifica se o valor que está dentro de num está dentre 1 e 100, e retorna o valor "true" ou "false"
 function  isNumero(n){
 if(Number(n)>=1 && Number(n) <= 100){
     return true;
@@ -11,7 +11,7 @@ if(Number(n)>=1 && Number(n) <= 100){
 return false;
 }
 }
-//Essa função verifica se o valor está dentro de vetor, se o valor digitado for diferente de -1(valor dado quando o valor digitado não tem idice valido, indice valido começa apartir de 0 e vai somando)
+//Essa função verifica se o valor está dentro de vetor, se o valor digitado for diferente de -1(valor dado quando o valor digitado não tem indice valido, indice valido começa apartir de 0 e vai somando)
 function  inlist(n,l){
 if(l.indexOf(Number(n)) != -1){
     return true;
@@ -23,6 +23,7 @@ if(l.indexOf(Number(n)) != -1){
 function adicionar(){
 //só vai adicionar se o valor estiver o numero em num && e só vai adcionar se ele "!" não estiver na lista
 if(isNumero(num.value) && !inlist(num.value, valores)){
+    
     //adiciona o valor contido no input "num" ao vetor chamado valores, o ,push adiiona o valor na ultima posição do vetor, então sempre que adicionarem valor no input, ele vai para ultima posição do vertor pois colocamos o nome do vetor ".push"
     valores.push(Number(num.value));
 
